247b46a16b0db1dac5453df3bd99c28b2fe37424
Simonas Tumsys <simonas.tumsys@gmail.com>
2024-10-13 23:49:23 +0300

    excluded node modules

---
+++ exercises/2.js
@@ -0,0 +1,5 @@
+/*
+Create an object that represents a person and assign it to a variable.
+It should have name (string), surname (string), age (integer), sex (string),
+isEmployed (boolean) properties. Also, 
+ */
